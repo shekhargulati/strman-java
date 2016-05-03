c8242324bf4d3ab305570fd784fc9fefe846737d
Shekhar Gulati <shekhargulati84@gmail.com>
2016-05-03 18:53:39 +0530

    project init

---
+++ src/main/java/strman/Strman.java
@@ -0,0 +1,9 @@
+package strman;
+
+/**
+ * A String manipulation library without any dependencies
+ */
+public interface Strman {
+
+
+}
---
+++ src/test/java/strman/StrmanTest.java
@@ -0,0 +1,10 @@
+package strman;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class StrmanTest {
+
+
+}
