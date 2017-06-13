a5d0bff55f5c88e8df76852f7d07b4e743774a5e
César Aguilera <cesar.aguilera.p@gmail.com>
2017-06-13 19:08:13 +0100

    #72: added fifth test

--- src/test/java/strman/StrmanTest.java
+++ src/test/java/strman/StrmanTest.java
@@ -1081,14 +1081,20 @@ public class StrmanTest {
     }
 
     @Test
-    public void zip_shouldReturnListOfPairs_whenBothInputHaveSameSize(){
+    public void zip_shouldReturnListOfPairs_whenBothInputHaveSameSize() {
         assertThat(zip("abc", "def"), equalTo(asList("ad", "be", "cf")));
         assertThat(zip("ABC", "DEF"), equalTo(asList("AD", "BE", "CF")));
     }
 
     @Test
-    public void zip_shouldReturnListOfPairs_whenFirstInputIsBiggerThanSecond(){
+    public void zip_shouldReturnListOfPairs_whenFirstInputIsBiggerThanSecond() {
         assertThat(zip("abc", "d"), equalTo(asList("ad")));
         assertThat(zip("ABCDE", "FGH"), equalTo(asList("AF", "BG", "CH")));
+    }
+
+    @Test
+    public void zip_shouldReturnListOfPairs_whenSecondInputIsBiggerThanFirst() {
+        assertThat(zip("d", "abc"), equalTo(asList("da")));
+        assertThat(zip("FGH", "ABCDE"), equalTo(asList("FA", "GB", "HC")));
     }
 }
