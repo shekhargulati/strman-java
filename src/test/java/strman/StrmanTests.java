9fb07aa1f8928b9ab936aed43c4f9ce84d28a997
Francesca Guiducci <guiducci.francesca@gmail.com>
2017-10-22 00:39:40 -0700

    Add escapeRegExp function (#95)

--- src/test/java/strman/StrmanTests.java
+++ src/test/java/strman/StrmanTests.java
@@ -1212,4 +1212,30 @@ public class StrmanTests {
         assertThat(startCase("--dashes--"), equalTo("Dashes"));
         assertThat(startCase("dashes----between----words"), equalTo("Dashes Between Words"));
     }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void escapeRegExp_shouldThrowException() throws Exception {
+        escapeRegExp(null);
+    }
+
+    @Test
+    public void escapeRegExp_shouldEscapeRegExp() throws Exception {
+        assertThat(escapeRegExp("\\"), equalTo("\\\\"));
+        assertThat(escapeRegExp("^"), equalTo("\\^"));
+        assertThat(escapeRegExp("$"), equalTo("\\$"));
+        assertThat(escapeRegExp("*"), equalTo("\\*"));
+        assertThat(escapeRegExp("+"), equalTo("\\+"));
+        assertThat(escapeRegExp("-"), equalTo("\\-"));
+        assertThat(escapeRegExp("?"), equalTo("\\?"));
+        assertThat(escapeRegExp("."), equalTo("\\."));
+        assertThat(escapeRegExp("|"), equalTo("\\|"));
+        assertThat(escapeRegExp("("), equalTo("\\("));
+        assertThat(escapeRegExp(")"), equalTo("\\)"));
+        assertThat(escapeRegExp("{"), equalTo("\\{"));
+        assertThat(escapeRegExp("}"), equalTo("\\}"));
+        assertThat(escapeRegExp("["), equalTo("\\["));
+        assertThat(escapeRegExp("]"), equalTo("\\]"));
+        assertThat(escapeRegExp("How much is (2+3)? 5"), equalTo("How much is \\(2\\+3\\)\\? 5"));
+        assertThat(escapeRegExp("\\s|_|-|(?<=[a-z])(?=[A-Z])"), equalTo("\\\\s\\|_\\|\\-\\|\\(\\?<=\\[a\\-z\\]\\)\\(\\?=\\[A\\-Z\\]\\)"));
+    }
 }
